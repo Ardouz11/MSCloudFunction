78ae6367bdee4ea9014a9e1dec08bddc21e359a3
rachid ardouz <rachid.ardouz@inwi.ma>
2022-03-17 15:46:18 +0100

    Supplier

--- src/main/java/com/spring/cloud/MsSpringCloudApplication.java
+++ src/main/java/com/spring/cloud/MsSpringCloudApplication.java
@@ -1,6 +1,14 @@
 package com.spring.cloud;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
+import java.util.function.Consumer;
 import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -16,4 +24,18 @@ public class MsSpringCloudApplication {
 	public Function<String, String> uppercase() {
 		return value -> value.toUpperCase();
 	}
+	@Bean
+	public Supplier<List<String>> getList() {
+		/*Stream<String> cities = Stream.of("Sydney", "Dhaka", "New York", "London");
+		 * 
+		 * 
+		 * To consume once
+		 */
+		ArrayList<String> cities = new ArrayList<String>();
+		cities.add("Sydney");
+		cities.add("Dhaka");
+		return () ->cities ;
+	}
+
+	
 }
